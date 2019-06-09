dfad4572923fa4b17586c21dabdfa47546522202
DmitroYatsuk <dyatsuk@gmail.com>
2019-06-09 23:21:09 +0300

    The Stone-Scissors-Paper Game has been developed

--- HW_2/js/main.js
+++ HW_2/js/main.js
@@ -1,3 +1,4 @@
+//The Stone-Scissors-Paper Game
 var btn = document.getElementById("play");
 var player1 = document.getElementById("player1");
 var player2 = document.getElementById("player2");
@@ -6,15 +7,59 @@ function getPlayerResult() {
     return Math.floor((Math.random() * 3) + 1);
 }
 
+function getNameById(playerResult) {
+    switch (playerResult) {
+        case 1:
+            return "Stone"
+            break;
+        case 2:
+            return "Scissors"
+            break;
+        case 3:
+            return "Paper"
+            break;
+    }
 
-function runGame() {
-    
-    player1.innerHTML = getPlayerResult();
-    player2.innerHTML = getPlayerResult();
-    
-    
 }
 
+function determineWinner(playerResult1, playerResult2) {
+    if ((playerResult1 == 1 && playerResult2 == 2) ||
+        (playerResult1 == 2 && playerResult2 == 3) ||
+        (playerResult1 == 3 && playerResult2 == 1)) {
+        return 1;
+    }
+    else if (playerResult1 == playerResult2) {
+        return 0;
+    }
+    else {
+        return 2;
+    }
 
+}
+
+// Print result
+function printResult(winner, id = "result") {
+    let divId = document.getElementById(id);
+    switch (winner) {
+        case 0:
+            divId.innerHTML = "Results are equal. You should restart a game";
+            break;
+        case 1:
+            divId.innerHTML = "Player ONE is a winner";
+            break;
+        case 2:
+            divId.innerHTML = "Player TWO is a winner";
+            break;
+    }
+}
+
+function runGame() {
+    var resultPlayer1 = getPlayerResult();
+    var resultPlayer2 = getPlayerResult();
+    player1.innerHTML = getNameById(resultPlayer1);
+    player2.innerHTML = getNameById(resultPlayer2);
+
+    printResult(determineWinner(resultPlayer1, resultPlayer2));
+}
 
 btn.addEventListener("click", runGame);

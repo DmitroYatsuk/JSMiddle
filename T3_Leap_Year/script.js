a07e21a0f189efea3c7e3405570fb139ea4ec927
DmitroYatsuk <dyatsuk@gmail.com>
2019-06-10 22:33:52 +0300

    Initial commit

---
+++ T3_Leap_Year/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function isLeapYear () {
+  
+}
+
+//console.log(isLeapYear(1600));  //true
+//console.log(isLeapYear(2100));  //false
+//console.log(isLeapYear(2012)); //true
+//console.log(isLeapYear());    //Error - year is not found
+

034e9230d58189571334358c253075f0f35ce89a
DmitroYatsuk <dmytro.iatsuk@gmail.com>
2019-06-13 18:42:15 +0300

    Calculation algorithm has been modifyed

--- T1_Ravens/main.js
+++ T1_Ravens/main.js
@@ -3,23 +3,21 @@
  в зависимости от переданого числа n. Например: На ветке сидит 1 ворона;
  На ветке сидит 4 вороны; На ветке сидит 26 ворон.*/
 
-function getRavensNames(quantity) {
-    if (quantity == 1 || quantity == 21) {
-        console.log(`На ветке сидит ${quantity} ворона`);
+function getRavensNames(n) {
+    let plural = (n % 10 == 1 && n % 100 != 11 ? 0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2);
+    if (plural === 0) {
+        console.log(`На ветке сидит ${n} ворона`);
     }
-    else if (quantity == 2 || quantity == 3 || quantity == 4 || quantity == 22 || quantity == 23 || quantity == 24) {
-        console.log(`На ветке сидит ${quantity} вороны`);
+    else if (plural === 1) {
+        console.log(`На ветке сидит ${n} вороны`);
     }
-    else if (quantity == 0) {
-        console.log("На ветке нет ворон");
-    }
-    else {
-        console.log(`На ветке сидит ${quantity} ворон`);
+    else if (plural === 2) {
+        console.log(`На ветке сидит ${n} ворон`);
     }
 }
 
 // Tests
-getRavensNames(0);  // На ветке нет ворон
+getRavensNames(0);  // На ветке сидит 0 ворон
 getRavensNames(1);  // На ветке сидит 1 ворона
 getRavensNames(21);  // На ветке сидит 21 ворона
 getRavensNames(2);  // На ветке сидит 2 вороны

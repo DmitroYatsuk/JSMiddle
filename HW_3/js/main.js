6471707135363d3d4531a52e45c19bea46f0bb2b
DmitroYatsuk <dyatsuk@gmail.com>
2019-06-17 12:14:29 +0300

    Task #1. Element  #6 has been removed from array.

--- HW_3/js/main.js
+++ HW_3/js/main.js
@@ -1,16 +1,17 @@
 var btn = document.getElementById("play");
 
-function transform() {
-    
-    
-    
-   /*
-   ваша программа 
-   по возможности разбейте на функции
-   */ 
+function printResult(resultStr, id = "result") {
+   document.getElementById(id).innerHTML = resultStr;
+}
 
-    
-    
+function transform() {
+   /* 1. С помощью функции splice необходимо вырезать 6-й элемент массива. 
+   В результате ваш массив должен стать короче на один элемент. */
+   let removedData = data.splice(6, 1);
+   //printResult(removedData);
+   //printResult(data);
+
+   
 }
 
 

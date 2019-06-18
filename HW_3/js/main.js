64160648c1ce9eb0b8934b6eedec3b8c9d8f55a8
DmitroYatsuk <dmytro.iatsuk@gmail.com>
2019-06-18 13:58:47 +0300

    Code clean-up and strict mode has been switched on

--- HW_3/js/main.js
+++ HW_3/js/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var btn = document.getElementById("play");
 
 function printResult(resultStr) {
@@ -5,19 +7,20 @@ function printResult(resultStr) {
 }
 
 function transform() {
+   // Copy of source data
+   var copiedData = data.slice();
+
    /* 1. С помощью функции splice необходимо вырезать 6-й элемент массива. 
    В результате ваш массив должен стать короче на один элемент. */
-   let removedData = data.slice().splice(6, 1);
-   printResult(removedData);
-   console.log("End of removedData ==================");
-   printResult(data);
-   console.log("End of data ==================");
+
+   var removedData = copiedData.splice(6, 1);
 
    /* 2. Используйте функцию forEach.
    Внутри цикла создайте новый массив объектов.
    В процессе создания нового массива объектов, избавьтесь от ключа id. */
+
    var newArr = [];
-   data.forEach(function (item, index) {
+   copiedData.forEach(function (item, index) {
       newArr.push({
          url: item.url,
          name: item.name,
@@ -26,10 +29,29 @@ function transform() {
          date: item.date,
       })
    })
-   //printResult(newArr);
 
-   /* 3. По новому массиву объектов, 
-   полученному с помощью функции forEach пройдитесь методом map() */
+   /*
+   3. По новому массиву объектов, 
+   полученному с помощью функции forEach пройдитесь методом map()
+   4. На каждой итерации цикла мы получаем один объект из массива объектов. 
+   Берем этот объект и преобразоваем его поля по следующим правилам:
+   5. Для поля Name: с помощью функций работы со стрингами делаете первую букву большой, остальные маленькие (ДЖИП -> Джип)
+   6. Для поля url: добавить перед ним «http://»
+   7. Для поля Description: с помощью функций работы со стрингами делаете обрезание до 15 символов. 
+   После добавляем многоточие (…) Остальное отбрасываете.
+   8. Для поля date: создать объект даты из заданных миллисекунд и потом отобразить в виде «2015/07/02 14:15»
+   9*.(дополнительное задание)
+   Более предпочтительно работать с датой с помощью библиотеки moment.js
+   Постарайтесь разобраться как она работает и использовать ее вместо примера выше. Если очень тяжело — используйте подход выше.
+   10. Для поля params: из значений ключей сформировать строку типа «true=>80». Для выполнения задания можно обращаться к полям объект params напрямую.
+   То есть params.status и params.progress
+   11. Создать новое поле isVisible. Переложить в это поле значение поля params.status.
+   12. После всех преобразований функция map вернет вам новый массив. 
+   Теперь с помощью функции filter вам необходимо выбрать только те элементы у которых isVisible == true. 
+   Пример работы функции filter есть в презентации.
+   13. Полученный результат печатаем в консоль.
+   Для печати используем отдельную функцию как в прошлых заданиях. То есть вынесете console.log в отдельную функцию. */
+
    var mappedArr = newArr.map(item => {
       return {
          url: `http://${item.url}`,
@@ -38,12 +60,13 @@ function transform() {
          params: `${item.params.status}=>${item.params.progress}`,
          description: `${item.description}`.substring(0, 15),
          //description: String(item.description).substring(0, 15),
-         date: moment(item.date).format("YYYY/MM/DD hh:mm"),
+         date: moment(item.date).format("YYYY/MM/DD HH:mm"),
          //date: newDate(item.date),
          isVisible: item.params.status,
       }
    })
 
+   // Date formatting fn
    function newDate(date) {
       var tmpDate = new Date(date);
       return tmpDate.getFullYear() + "/" +
@@ -53,33 +76,8 @@ function transform() {
          tmpDate.getMinutes();
    }
 
-   printResult(mappedArr);
-   console.log("End of mappedArr ==================");
-
-   /*    9*.(дополнительное задание)
-   Более предпочтительно работать с датой с помощью библиотеки moment.js
-   Постарайтесь разобраться как она работает и использовать ее вместо примера выше. Если очень тяжело — используйте подход выше. */
-
-
-
-   /* 10. Для поля params: из значений ключей сформировать строку типа «true=>80». Для выполнения задания можно обращаться к полям объект params напрямую.
-   То есть params.status и params.progress */
-
-
-
-   /* 11. Создать новое поле isVisible. Переложить в это поле значение поля params.status. */
-
-   /* 12. После всех преобразований функция map вернет вам новый массив. Теперь с помощью функции filter вам необходимо выбрать только те элементы у которых isVisible == true. Пример работы функции filter есть в презентации. */
-
-   var filteredArr = mappedArr.filter(item => item.isVisible == true );
+   var filteredArr = mappedArr.filter(item => item.isVisible == true);
    printResult(filteredArr);
-   console.log("End of filteredArr ==================");
-
-
-   /* 13. Полученный результат печатаем в консоль.
-   Для печати используем отдельную функцию как в прошлых заданиях. То есть вынесете console.log в отдельную функцию. */
 }
-
-
 
 btn.addEventListener("click", transform);

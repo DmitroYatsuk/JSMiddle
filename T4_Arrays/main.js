85f925528f241f56f357def21da241e71a1a32cd
DmitroYatsuk <dmytro.iatsuk@gmail.com>
2019-06-20 14:21:05 +0300

    Task has been done

--- T4_Arrays/main.js
+++ T4_Arrays/main.js
@@ -6,20 +6,14 @@ let goods = ['foods', 'fruits', 'technics', 'phones', 'computers'];
 let deletedGoods = goods.splice(2,1);
 let strGoods = goods.join(", ");
 
-console.log(goods);
-console.log(deletedGoods);
 console.log(strGoods);
 
 /* 2. Преобразовать текущую дату и время в понятный человеку формат: 08:05 01/01/2018. Используя шаблонные строки.
  */
 
 function currentDate() {
-    var curDate = new Date();
-    return curDate.getHours() + ":" +
-        curDate.getMinutes() + " " +
-        curDate.getDate() + "/" +
-        curDate.getMonth() + "/" +
-        curDate.getFullYear();
+    let curDate = new Date();
+    return `${curDate.getHours()}:${curDate.getMinutes()} ${curDate.getDate()}/${curDate.getMonth()}/${curDate.getFullYear()}`
  }
 
 console.log(currentDate());
@@ -28,10 +22,16 @@ console.log(currentDate());
 Функция должна принимать строку
  */
 function getExt(path) {
-
+    return path.substr(path.indexOf(".") + 1);
 }
 
-getExt('/home/user/project/script.js');
+console.log(getExt('/home/user/project/script.js'));
 
 /* 4. Напишите функцию, которая удаляет дубликаты из массива. 
 Например, входной массив: [1, 2, 2, 4, 5, 4, 7, 8, 7, 3, 6], массив который возвращает функция [1, 2, 4, 5, 7, 8, 3, 6] */
+
+let arrIn = [1, 2, 2, 4, 5, 4, 7, 8, 7, 3, 6];
+
+let arrOut = arrIn.filter((item, idx) => {return arrIn.indexOf(item) === idx})
+
+console.log(arrOut);

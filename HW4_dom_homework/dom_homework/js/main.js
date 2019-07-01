00f62a0e61d76035c013ead66284e0ced7a492f0
DmitroYatsuk <dyatsuk@gmail.com>
2019-07-01 23:15:29 +0300

    Mapped array has been created

--- HW4_dom_homework/dom_homework/js/main.js
+++ HW4_dom_homework/dom_homework/js/main.js
@@ -6,37 +6,88 @@
         secondBlock = document.querySelector('#second-line'),
         thirdBlock = document.querySelector('#third-line');
 
-    let item = {
-        url: "http://desktopwallpapers.org.ua/mini/201507/40069.jpg",
-        name: "CHEVROLET",
-        description: "Lorem ipsum dolor sit amet",
-        date: "2015/01/25 14:15"
-    };
+    // Copy of source data
+    let copiedData = data.slice();
+
+    //Remove id and params from data[]
+    let newArr = [];
+    copiedData.forEach(item => {
+        newArr.push({
+            url: item.url,
+            name: item.name,
+            description: item.description,
+            date: item.date,
+        })
+    })
+
+    //Create new mapped array
+    let mappedArr = newArr.map(item => {
+        return {
+           url: `http://${item.url}`,
+           name: `${item.name}`.charAt(0).toLocaleUpperCase() + `${item.name}`.slice(1).toLowerCase(),
+           description: shrinkString(item.description),
+           //date: moment(item.date).format("YYYY/MM/DD HH:mm"),
+           date: newDate(item.date),
+
+        }
+     })
+  
+     // Date formatting fn
+     function newDate(date) {
+        let tmpDate = new Date(date);
+        return tmpDate.getFullYear() + "/" +
+           lessThanTen(tmpDate.getMonth() + 1) + "/" +
+           lessThanTen(tmpDate.getDate()) + " " +
+           lessThanTen(tmpDate.getHours()) + ":" +
+           lessThanTen(tmpDate.getMinutes());
+     }
+  
+     function lessThanTen(num) {
+        return num < 10 ? "0" + num : num;
+     }
+  
+     // Shrink string
+     function shrinkString(str) {
+        if (str.length >= 15) {
+           return str.substring(0, 15) + "...";
+        }
+        else {
+           return str;
+        }
+     }
+
+    // сначала снимаете значение с селектбокса, 
+    let typeSelector = document.getElementById("type-selector").value;
+    /* Определить количество элементов, которые будете показывать в галерее. 
+    Для этого снять значение со второго селектбокса. */
+    let lineSelector = document.getElementById("line-selector").value;
 
     function init() {
-        // Copy of source data
-        let copiedData = data.slice();
 
-        //Remove id and params from data[]
-        let newArr = [];
-        copiedData.forEach(item => {
-            newArr.push({
-                url: item.url,
-                name: item.name,
-                description: item.description,
-                date: item.date,
-            })
-        })
-
-        // сначала снимаете значение с селектбокса, 
-        let typeSelector = document.getElementById("type-selector").value;
         // определяете какой способ построения галлереи надо использовать
-        
+        switch (typeSelector) {
+            case 0:
+                break;
+            case 1:
+                //
+                break;
+            case 2:
+                //
+                break;
+            case 3:
+                //
+                break;
+            default:
+                break;
+        }
         // запускаете необходимую логику
+        for (let i = 0; i < lineSelector; i++) {
+            
+        }
 
         // код ниже дан для справки, вам нужно будет использовать тот вариан который вы выбрали в селектбоксе
         // пример построения галлереи с помощю replace
-        var replaceItemTemplate = '<div class="col-sm-3 col-xs-6">\
+        let replaceItemTemplate = '<div class="col-sm-3 col-xs-6">\
     <img src="$url" alt="$name" class="img-thumbnail">\
     <div class="info-wrapper">\
     <div class="text-muted">$name</div>\

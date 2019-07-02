720799dcab6f67813aadf59a8666dd26ab55392e
DmitroYatsuk <dmytro.iatsuk@gmail.com>
2019-07-02 19:04:19 +0300

    Gallary drawing by Str replace and Str  template

--- HW4_dom_homework/dom_homework/js/main.js
+++ HW4_dom_homework/dom_homework/js/main.js
@@ -23,103 +23,123 @@
     //Create new mapped array
     let mappedArr = newArr.map(item => {
         return {
-           url: `http://${item.url}`,
-           name: `${item.name}`.charAt(0).toLocaleUpperCase() + `${item.name}`.slice(1).toLowerCase(),
-           description: shrinkString(item.description),
-           //date: moment(item.date).format("YYYY/MM/DD HH:mm"),
-           date: newDate(item.date),
-
+            url: `http://${item.url}`,
+            name: `${item.name}`.charAt(0).toLocaleUpperCase() + `${item.name}`.slice(1).toLowerCase(),
+            description: shrinkString(item.description),
+            //date: moment(item.date).format("YYYY/MM/DD HH:mm"),
+            date: newDate(item.date),
         }
-     })
-  
-     // Date formatting fn
-     function newDate(date) {
+    })
+
+    // Date formatting fn
+    function newDate(date) {
         let tmpDate = new Date(date);
         return tmpDate.getFullYear() + "/" +
-           lessThanTen(tmpDate.getMonth() + 1) + "/" +
-           lessThanTen(tmpDate.getDate()) + " " +
-           lessThanTen(tmpDate.getHours()) + ":" +
-           lessThanTen(tmpDate.getMinutes());
-     }
-  
-     function lessThanTen(num) {
+            lessThanTen(tmpDate.getMonth() + 1) + "/" +
+            lessThanTen(tmpDate.getDate()) + " " +
+            lessThanTen(tmpDate.getHours()) + ":" +
+            lessThanTen(tmpDate.getMinutes());
+    }
+
+    function lessThanTen(num) {
         return num < 10 ? "0" + num : num;
-     }
-  
-     // Shrink string
-     function shrinkString(str) {
+    }
+
+    // Shrink string
+    function shrinkString(str) {
         if (str.length >= 15) {
-           return str.substring(0, 15) + "...";
+            return str.substring(0, 15) + "...";
         }
         else {
-           return str;
+            return str;
         }
-     }
-
-    // сначала снимаете значение с селектбокса, 
-    let typeSelector = document.getElementById("type-selector").value;
-    /* Определить количество элементов, которые будете показывать в галерее. 
-    Для этого снять значение со второго селектбокса. */
-    let lineSelector = document.getElementById("line-selector").value;
+    }
 
     function init() {
-
-        // определяете какой способ построения галлереи надо использовать
-        switch (typeSelector) {
-            case 0:
+        let galleryElemQuantity;
+        // Определить количество элементов, которые будете показывать в галерее. 
+        let lineSelector = document.getElementById("line-selector").value;
+        switch (lineSelector) {
+            case "0":
+                galleryElemQuantity = mappedArr.length;
                 break;
-            case 1:
-                //
+            case "1":
+                galleryElemQuantity = 3;
                 break;
-            case 2:
-                //
-                break;
-            case 3:
-                //
+            case "2":
+                galleryElemQuantity = 6;
                 break;
             default:
                 break;
         }
-        // запускаете необходимую логику
-        for (let i = 0; i < lineSelector; i++) {
-            
+        // сначала снимаете значение с селектбокса, 
+        let typeSelector = document.getElementById("type-selector").value;
+        switch (typeSelector) {
+            case 0:
+                break;
+            case "1":
+                replaceStrings();
+                break;
+            case "2":
+                templateStrings();
+                break;
+            case "3":
+                createElement();
+                break;
+            default:
+                break;
         }
 
-        // код ниже дан для справки, вам нужно будет использовать тот вариан который вы выбрали в селектбоксе
-        // пример построения галлереи с помощю replace
-        let replaceItemTemplate = '<div class="col-sm-3 col-xs-6">\
-    <img src="$url" alt="$name" class="img-thumbnail">\
-    <div class="info-wrapper">\
-    <div class="text-muted">$name</div>\
-    <div class="text-muted top-padding">$description</div>\
-    <div class="text-muted">$date</div>\
-    </div>\
-    </div>';
+        //с помощю replace
+        function replaceStrings() {
+            let resultHTML = "";
+            for (let i = 0; i < galleryElemQuantity; i++) {
+                let replaceItemTemplate = '<div class="col-sm-3 col-xs-6">\
+                    <img src="$url" alt="$name" class="img-thumbnail">\
+                    <div class="info-wrapper">\
+                    <div class="text-muted">$name</div>\
+                    <div class="text-muted top-padding">$description</div>\
+                    <div class="text-muted">$date</div>\
+                    </div>\
+                    </div>';
+                let tempItem = replaceItemTemplate
+                    .replace(/\$name/gi, mappedArr[i].name)
+                    .replace("$url", mappedArr[i].url)
+                    .replace("$description", mappedArr[i].description)
+                    .replace("$date", mappedArr[i].date);
+                resultHTML += tempItem;
+            }
+            firstBlock.innerHTML = resultHTML;
+            document.querySelector('.first-group').classList.add("show");
+            document.querySelector('.second-group').classList.add("hide");
+            document.querySelector('.third-group').classList.add("hide");
+        }
 
-        let resultHTML = replaceItemTemplate
-            .replace(/\$name/gi, item.name)
-            .replace("$url", item.url)
-            .replace("$description", item.description)
-            .replace("$date", item.date);
+        function templateStrings() {
+            let resultHTML = "";
+            for (let i = 0; i < galleryElemQuantity; i++) {
+                let tempItem = `<div class="col-sm-3 col-xs-6">\
+                <img src="${mappedArr[i].url}" alt="${mappedArr[i].name}" class="img-thumbnail">\
+                <div class="info-wrapper">\
+                <div class="text-muted">${mappedArr[i].name}</div>\
+                <div class="text-muted top-padding">${mappedArr[i].description}</div>\
+                <div class="text-muted">${mappedArr[i].date}</div>\
+                </div>\
+                </div>`;
+                resultHTML += tempItem;
+            }
+            secondBlock.innerHTML = resultHTML;
+            document.querySelector('.first-group').classList.add("hide");
+            document.querySelector('.second-group').classList.add("show");
+            document.querySelector('.third-group').classList.add("hide");
+        }
 
-        firstBlock.innerHTML = resultHTML;
-
-        // один из примеров как прятать блоки
-        document.querySelector('.first-group').classList.add("show");
-        document.querySelector('.second-group').classList.add("hide");
-        document.querySelector('.third-group').classList.add("hide");
-
-        // пример построения галлереи с помощю шаблонных строк
-        let secondItemTemplate = `<div class="col-sm-3 col-xs-6">\
-    <img src="${item.url}" alt="${item.name}" class="img-thumbnail">\
-    <div class="info-wrapper">\
-        <div class="text-muted">${item.name}</div>\
-        <div class="text-muted top-padding">${item.description}</div>\
-        <div class="text-muted">${item.date}</div>\
-    </div>\
-    </div>`;
-        //secondBlock.innerHTML = secondItemTemplate;	
-
+        function createElement() {
+            thirdBlock.innerHTML = thirdItemTemplate;
+            document.querySelector('.first-group').classList.add("hide");
+            document.querySelector('.second-group').classList.add("hide");
+            document.querySelector('.third-group').classList.add("show");
+        }
     }
 
     btn.addEventListener("click", init);

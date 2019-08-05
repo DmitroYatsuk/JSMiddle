5515752e3b0c7ea1c092a03115c1c4095efcefeb
DmitroYatsuk <dyatsuk@gmail.com>
2019-08-06 00:13:39 +0300

    Code refactoring has been done

--- HW4_dom_homework/dom_homework/js/main.js
+++ HW4_dom_homework/dom_homework/js/main.js
@@ -1,14 +1,94 @@
 'use strict';
 (function () {
     const btn = document.getElementById("play"),
-        firstBlock = document.querySelector('#first-line'),
-        secondBlock = document.querySelector('#second-line'),
-        thirdBlock = document.querySelector('#third-line'),
-        firstGroup = document.querySelector('.first-group'),
-        secondGroup = document.querySelector('.second-group'),
-        thirdGroup = document.querySelector('.third-group'),
+        firstBlock = document.getElementById('first-line'),
+        secondBlock = document.getElementById('second-line'),
+        thirdBlock = document.getElementById('third-line'),
         lineSelector = document.getElementById("line-selector"),
-        typeSelector = document.getElementById("type-selector");
+        typeSelector = document.getElementById("type-selector"),
+        groupList = document.querySelectorAll('.display-group');
+
+    // Shrink string
+    function shrinkString(str) {
+                return (str.length >= 15)
+                    ? str.substring(0, 15) + "..."
+                    : str;
+        }
+
+    function showBlock(elementNumber, groupList) {
+        for (let i = 0; i < groupList.length; i++) {
+        groupList[i].classList.remove('show');
+        groupList[i].classList.add('hide');
+        }
+        groupList[elementNumber].classList.add('show');
+    }
+
+    function replaceStrings(elements, mappedArr) {
+        let resultHTML = "";
+        for (let i = 0; i < elements; i++) {
+            let replaceItemTemplate = '<div class="col-sm-3 col-xs-6">\
+                <img src="$url" alt="$name" class="img-thumbnail">\
+                <div class="info-wrapper">\
+                <div class="text-muted">$name</div>\
+                <div class="text-muted top-padding">$description</div>\
+                <div class="text-muted">$date</div>\
+                </div>\
+                </div>';
+            resultHTML += replaceItemTemplate
+                .replace(/\$name/gi, mappedArr[i].name)
+                .replace("$url", mappedArr[i].url)
+                .replace("$description", mappedArr[i].description)
+                .replace("$date", mappedArr[i].date);
+        }
+        firstBlock.innerHTML = resultHTML;
+        showBlock(1, groupList);
+    }
+
+    function templateStrings(elements, mappedArr) {
+        let resultHTML = "";
+        for (let i = 0; i < elements; i++) {
+            let tempItem = `<div class="col-sm-3 col-xs-6">\
+            <img src="${mappedArr[i].url}" alt="${mappedArr[i].name}" class="img-thumbnail">\
+            <div class="info-wrapper">\
+            <div class="text-muted">${mappedArr[i].name}</div>\
+            <div class="text-muted top-padding">${mappedArr[i].description}</div>\
+            <div class="text-muted">${mappedArr[i].date}</div>\
+            </div>\
+            </div>`;
+            resultHTML += tempItem;
+        }
+        secondBlock.innerHTML = resultHTML;
+        showBlock(2, groupList);
+    }
+
+    function createElement(elements, mappedArr) {
+        for (let i = 0; i < elements; i++) {
+            const divOne = document.createElement("div"),
+                divTwo = document.createElement("div"),
+                divThree = document.createElement("div"),
+                divFour = document.createElement("div"),
+                divFive = document.createElement("div"),
+                image = document.createElement("img");
+            divOne.classList.add("col-sm-3", "col-xs-6");
+            image.src = mappedArr[i].url;
+            image.alt = mappedArr[i].name;
+            image.classList.add("img-thumbnail");
+            divTwo.classList.add("info-wrapper");
+            divThree.classList.add("text-muted");
+            divThree.textContent = mappedArr[i].name;
+            divFour.classList.add("text-muted", "top-padding");
+            divFour.textContent = mappedArr[i].description;
+            divFive.classList.add("text-muted");
+            divFive.textContent = mappedArr[i].date;
+            divTwo.appendChild(divThree);
+            divTwo.appendChild(divFour);
+            divTwo.appendChild(divFive);
+            divOne.appendChild(image);
+            divOne.appendChild(divTwo);
+            thirdBlock.appendChild(divOne);
+        }
+        showBlock(3, groupList);
+    }
 
     function init() {
         // Copy of source data
@@ -29,21 +109,12 @@
         let mappedArr = newArr.map(item => {
             return {
                 url: `http://${item.url}`,
-                name: `${item.name}`.charAt(0).toLocaleUpperCase() + `${item.name}`.slice(1).toLowerCase(),
+                name: item.name.charAt(0).toLocaleUpperCase() + `${item.name}`.slice(1).toLowerCase(),
                 description: shrinkString(item.description),
                 date: moment(item.date).format("YYYY/MM/DD HH:mm"),
             }
         })
 
-        // Shrink string
-        function shrinkString(str) {
-            if (str.length >= 15) {
-                return str.substring(0, 15) + "...";
-            }
-            else {
-                return str;
-            }
-        }
         let galleryElemQuantity;
         // Определить количество элементов, которые будете показывать в галерее. 
         switch (lineSelector.value) {
@@ -60,90 +131,15 @@
         // сначала снимаете значение с селектбокса, 
         switch (typeSelector.value) {
             case "1":
-                replaceStrings();
+                replaceStrings(galleryElemQuantity, mappedArr);
                 break;
             case "2":
-                templateStrings();
+                templateStrings(galleryElemQuantity, mappedArr);
                 break;
             case "3":
-                createElement();
+                createElement(galleryElemQuantity, mappedArr);
                 break;
-        }
-
-        function replaceStrings() {
-            let resultHTML = "";
-            for (let i = 0; i < galleryElemQuantity; i++) {
-                let replaceItemTemplate = '<div class="col-sm-3 col-xs-6">\
-                    <img src="$url" alt="$name" class="img-thumbnail">\
-                    <div class="info-wrapper">\
-                    <div class="text-muted">$name</div>\
-                    <div class="text-muted top-padding">$description</div>\
-                    <div class="text-muted">$date</div>\
-                    </div>\
-                    </div>';
-                resultHTML += replaceItemTemplate
-                    .replace(/\$name/gi, mappedArr[i].name)
-                    .replace("$url", mappedArr[i].url)
-                    .replace("$description", mappedArr[i].description)
-                    .replace("$date", mappedArr[i].date);
-            }
-            firstBlock.innerHTML = resultHTML;
-            firstGroup.classList.toggle("show");
-            secondGroup.classList.remove("show");
-            thirdGroup.classList.remove("show");
-        }
-
-        function templateStrings() {
-            let resultHTML = "";
-            for (let i = 0; i < galleryElemQuantity; i++) {
-                let tempItem = `<div class="col-sm-3 col-xs-6">\
-                <img src="${mappedArr[i].url}" alt="${mappedArr[i].name}" class="img-thumbnail">\
-                <div class="info-wrapper">\
-                <div class="text-muted">${mappedArr[i].name}</div>\
-                <div class="text-muted top-padding">${mappedArr[i].description}</div>\
-                <div class="text-muted">${mappedArr[i].date}</div>\
-                </div>\
-                </div>`;
-                resultHTML += tempItem;
-            }
-            secondBlock.innerHTML = resultHTML;
-            firstGroup.classList.remove("show");
-            secondGroup.classList.toggle("show");
-            thirdGroup.classList.remove("show");
-        }
-
-        function createElement() {
-            for (let i = 0; i < galleryElemQuantity; i++) {
-                const divOne = document.createElement("div"),
-                    divTwo = document.createElement("div"),
-                    divThree = document.createElement("div"),
-                    divFour = document.createElement("div"),
-                    divFive = document.createElement("div"),
-                    image = document.createElement("img");
-                divOne.classList.add("col-sm-3", "col-xs-6");
-                image.src = mappedArr[i].url;
-                image.alt = mappedArr[i].name;
-                image.classList.add("img-thumbnail");
-                divTwo.classList.add("info-wrapper");
-                divThree.classList.add("text-muted");
-                divThree.textContent = mappedArr[i].name;
-                divFour.classList.add("text-muted", "top-padding");
-                divFour.textContent = mappedArr[i].description;
-                divFive.classList.add("text-muted");
-                divFive.textContent = mappedArr[i].date;
-                divTwo.appendChild(divThree);
-                divTwo.appendChild(divFour);
-                divTwo.appendChild(divFive);
-                divOne.appendChild(image);
-                divOne.appendChild(divTwo);
-                thirdBlock.appendChild(divOne);
-            }
-            firstGroup.classList.remove("show");
-            secondGroup.classList.remove("show");
-            thirdGroup.classList.toggle("show");
-        }
+        }        
     }
-
     btn.addEventListener("click", init);
-
 })()

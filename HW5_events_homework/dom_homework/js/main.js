83670c92dd5d42c817c9dbceaedcbcfe590c472f
DmitroYatsuk <dyatsuk@gmail.com>
2019-08-12 08:25:17 +0300

    Global vars have been added

--- HW5_events_homework/dom_homework/js/main.js
+++ HW5_events_homework/dom_homework/js/main.js
@@ -1,78 +1,93 @@
 'use strict';
+let counter = 0,
+arrToDisplay = [];
+
 (function () {
     const addBtn = document.getElementById("add"),
-        removeBtn = document.getElementsByClassName('btn'),
+        //removeBtn = document.getElementsByClassName('btn'),
         result = document.getElementById('result'),
-        filterSelector = document.getElementById("filter-selector"),
+        //filterSelector = document.getElementById("filter-selector"),
         count = document.getElementById("count");
-        //groupList = document.querySelectorAll('.row[class*=group]');
-    
-    let counter = 0,
-        arrToDisplay = [];
+        //removeBtnList = document.querySelectorAll('.btn[class*=danger]');
 
     // Shrink string
     function shrinkString(str) {
-                return (str.length >= 15)
-                    ? str.substring(0, 15) + "..."
-                    : str;
-        }
+        return (str.length >= 15)
+            ? str.substring(0, 15) + "..."
+            : str;
+    }
 
-    function showResult(mappedArr) {
-        for (let i = 0; i < mappedArr.length; i++) {
+    function showResult(arr, cnt) {
         let resultHTML = "";
-            resultHTML += `<div class="col-sm-3 col-xs-6">\
-            <img src="${mappedArr[i].url}" alt="${mappedArr[i].name}" class="img-thumbnail">\
+        for (let i = 0; i < arr.length; i++) {
+            resultHTML += `<div class="col-md-3 col-sm-4 col-xs-6">\
+            <img src="${arr[i].url}" alt="${arr[i].name}" class="img-thumbnail">\
             <div class="info-wrapper">\
-            <div class="text-muted">${mappedArr[i].name}</div>\
-            <div class="text-muted top-padding">${mappedArr[i].description}</div>\
-            <div class="text-muted">${mappedArr[i].date}</div>\
+            <div class="text-muted">${arr[i].name}</div>\
+            <div class="text-muted top-padding">${arr[i].description}</div>\
+            <div class="text-muted">${arr[i].date}</div>\
+            <button class="btn btn-danger">Удалить</button>\
             </div>\
-            </div>
-            <button class="btn btn-danger" onclick="removeElement(event)">Удалить</button>`;
-    }
+            </div>`;
+        }
         result.innerHTML = resultHTML;
-        count.innerHTML = counter;
+        count.innerHTML = cnt;
     }
 
-    function init() {
-        // Copy of source data
-        let copiedData = data.slice();
+    function addElement(mappedArr) {
+        if (counter < 10) {
+            arrToDisplay.push(mappedArr[counter]);
+            counter += 1;
+            showResult(arrToDisplay, counter);
+        }
+        else alert("Sorry, no more elements.");
+    }
 
-        //Remove id and params from data[]
-        let newArr = [];
-        copiedData.forEach(item => {
-            newArr.push({
-                url: item.url,
-                name: item.name,
-                description: item.description,
-                date: item.date,
-            })
+    let addBtnHandler = function (event) {
+        addElement(mappedArr);
+    }
+
+    function removeElement(mappedArr) {
+        if (counter >= 0) {
+            arrToDisplay = mappedArr.filter(item => item. == true);
+            counter += 1;
+            showResult(arrToDisplay, counter);
+        }
+        else alert("Sorry, no more elements.");
+    }   
+
+    let removeBtnHandler =  function (event) {
+        
+    }
+
+    //    function init() {
+    // Copy of source data
+    let copiedData = data.slice();
+
+    //Remove id and params from data[]
+    let newArr = [];
+    copiedData.forEach(item => {
+        newArr.push({
+            url: item.url,
+            name: item.name,
+            description: item.description,
+            date: item.date,
         })
+    })
 
-        //Create new mapped array
-        let mappedArr = newArr.map(item => {
-            return {
-                url: `http://${item.url}`,
-                name: item.name.charAt(0).toLocaleUpperCase() + `${item.name}`.slice(1).toLowerCase(),
-                description: shrinkString(item.description),
-                date: moment(item.date).format("YYYY/MM/DD HH:mm"),
-            }
-        })
-   }
+    //Create new mapped array
+    let mappedArr = newArr.map(item => {
+        return {
+            url: `http://${item.url}`,
+            name: item.name.charAt(0).toLocaleUpperCase() + `${item.name}`.slice(1).toLowerCase(),
+            description: shrinkString(item.description),
+            date: moment(item.date).format("YYYY/MM/DD HH:mm"),
+        }
+    })
+    //  }
 
-   function addElement(mappedArr) {
-       
-
-       counter += 1;
-       showResult();
-   }
-
-   function removeElement() {
-       showResult();
-   }
-
-    init();
-    addBtn.addEventListener("click", addElement);
-    result.addEventListener("click", removeElement);
+    //   init();
+    addBtn.addEventListener("click", addBtnHandler);
+    result.addEventListener("click", removeBtnHandler);
 
 })();

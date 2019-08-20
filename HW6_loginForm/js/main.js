56c1263fcdbc218011c1df0a2dc33ecd5a45c02c
DmitroYatsuk <dmytro.iatsuk@gmail.com>
2019-08-20 18:22:18 +0300

    Object interface has been added

--- HW6_loginForm/js/main.js
+++ HW6_loginForm/js/main.js
@@ -1,12 +1,26 @@
-(function () {
+const login = (function () {
     const loginInput = document.getElementById("inputEmail"),
         passwordInput = document.getElementById("inputPassword"),
         submitBtn = document.querySelector(".btn btn-lg btn-primary btn-block");
 
+    localStorage['login'] = "my@mail.com";
+    localStorage['pwd'] = "password";
+
+    function initComponent() {
+
+    }
+
+    function setLogAndPass() {
+
+    }
 
 
-    setLogAndPass();
-    initComponent();
+    return {
+        initComponent: initComponent,
+        setLogAndPass: setLogAndPass
+    }
+
+
     loginInput.addEventListener("click", loginHandler);
     passwordInput.addEventListener("click", passwordHandler);
     submitBtn.addEventListener("click", submitHandler);

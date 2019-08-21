377a17eb8ed7b3baf32dd847e879474756a8a5d6
DmitroYatsuk <dmytro.iatsuk@gmail.com>
2019-08-21 19:12:06 +0300

    Some changes have been added

--- HW6_loginForm/js/main.js
+++ HW6_loginForm/js/main.js
@@ -1,27 +1,74 @@
 const login = (function () {
     const loginInput = document.getElementById("inputEmail"),
         passwordInput = document.getElementById("inputPassword"),
-        submitBtn = document.querySelector(".btn btn-lg btn-primary btn-block");
-
-    localStorage['login'] = "my@mail.com";
-    localStorage['pwd'] = "password";
+        checkbox = document.getElementById("checkbox"),
+        alert = document.getElementById("alert"),
+        submitBtn = document.getElementById("submit");
 
     function initComponent() {
+        localStorage['login'] = "my@mail.com";
+        localStorage['pwd'] = "password";
+    }
+
+    function validateEmail(email) {
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
+
+    function inputFormValidation (login, pwd) {
+        //console.log(login, pwd);
+        if (login !== "" && pwd !== "") {
+            alert.classList.remove("show");
+            alert.classList.add("hide");
+            if (validateEmail(login)) {
+                if (localStorage['login'] === login && localStorage['pwd'] === pwd) {
+                    ///
+                }
+            }
+            else {
+                alert.innerText = "Wrong login format!"
+                alert.classList.remove("hide");
+                alert.classList.add("show");
+            }
+
+        }
+        else {
+            alert.innerText = "Wrong credentials!"
+            alert.classList.remove("hide");
+            alert.classList.add("show");
+        }
+    }
+
+    function setLogAndPass(login, pwd) {
+        console.log(login.value, pwd.value);
 
     }
 
-    function setLogAndPass() {
 
+
+
+    let loginHandler = function (e) {
+
+        console.log(e.target.value);
+    }
+
+    let passwordHandler = function (e) {
+        console.log(e.target.value);
+    }
+
+    let submitHandler = function (e) {
+        console.log(e.target);
+        inputFormValidation(loginInput.value, passwordInput.value);
     }
 
 
-    return {
+//    loginInput.addEventListener("change", loginHandler);
+//    passwordInput.addEventListener("change", passwordHandler);
+    submitBtn.addEventListener("click", submitHandler);
+
+
+/*     return {
         initComponent: initComponent,
         setLogAndPass: setLogAndPass
-    }
-
-
-    loginInput.addEventListener("click", loginHandler);
-    passwordInput.addEventListener("click", passwordHandler);
-    submitBtn.addEventListener("click", submitHandler);
+    } */
 })();
